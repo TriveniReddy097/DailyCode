881e82187f2fafc75e52a4335455657e567b1d33
Triveni Gudidoddi <trivenigudidoddi@Trivenis-MacBook-Pro.local>
2024-01-28 15:09:06 -0500

    day3: Longest common subsequence reccursion way

--- day3/LCS.cpp
+++ day3/LCS.cpp
@@ -19,6 +19,17 @@ public:
         }
         return dp[text1.length()][text2.length()];
     }
+
+    int LCM(string text1, string text2, int i,int j) {
+        if(i<0 || j<0) {
+            return 0;
+        }
+        if(text1[i] == text2[j]) {
+            return 1+LCM(text1, text2,i-1,j-1);
+        } else {
+            return max(LCM(text1, text2,i-1,j), LCM(text1, text2,i,j-1));
+        }
+    }
 };
 
 int main() {
@@ -27,6 +38,7 @@ int main() {
     string s2;
     cin>>s1;
     cin>>s2;
-    cout<<"Longest Common Subsequence is: "<<sol.longestCommonSubsequence(s1, s2);
+    cout<<"Longest Common Subsequence is: "<<sol.longestCommonSubsequence(s1, s2)<<endl; // DP approach
+    cout<<"Longest Common Subsequence is: "<<sol.LCM(s1, s2, s1.length()-1, s2.length()-1); //time limit exceeded if length of strings is more
     return 0;
 }
